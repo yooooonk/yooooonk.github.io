01abc72192ab0fd8a30dc5d2f82a42c2c2871c2d
ouo_yoonk <joyk9251@gmail.com>
2021-02-06 01:25:35 +0900

    큐브, 메뉴이름 크로스

--- js/cube.js
+++ js/cube.js
@@ -1,22 +1,34 @@
+const room = document.querySelector('.room');
 const cube = document.querySelector('.cube');
+const title = document.querySelector('.title-word-wrap');
 
-let cubeScroll = 1;
+
 let scrollState = false;
-let lastScroll = 0;
+let lastPage = 1;
+let mousePos = {x:0, y:0}
 
 window.addEventListener('scroll',(e)=>{
     clearTimeout(scrollState);
     
     const scrollPer = pageYOffset / maxScrollValue * 100;
-    rollCube(scrollPer);
+    const page = Math.floor(scrollPer/30);
+
+    rollCube(page);
+    rollTitle(page);
     
 });
 
-const rollCube = (scrollPer)=>{
+const rollCube = (page)=>{    
+        
+    cube.style.transform = `rotateX(${-90*page}deg)`    
+}
+
+const rollTitle = (page)=>{
     
-    console.log(scrollPer)
-    
-    cube.style.transform = `rotateX(${-90*cubeScroll}deg)`
+    if(lastPage !== page){
+        '페이지바뀜'
+    }
+    title.style.transform = `translateX(-${30*page}vw)`
 }
 
 window.addEventListener('resize',()=>{
@@ -28,3 +40,15 @@ const setMaxScrollValue = ()=>{
 }
 
 setMaxScrollValue();
+
+window.addEventListener('mousemove',(e)=>{
+    moveScreen(e);
+});
+
+const moveScreen = (e)=>{
+    
+    mousePos.x = -1 + (e.clientX/window.innerWidth)*2
+    mousePos.y = 1 - (e.clientY/window.innerHeight)*2;
+    
+    room.style.transform = `rotateX(${mousePos.y*10}deg) rotateY(${mousePos.x*10}deg)`        
+}
