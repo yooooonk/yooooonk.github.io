808fb3686f2be3495f8c11463f1fa7a33e1e1322
ouo_yoonk <joyk9251@gmail.com>
2021-02-24 23:41:33 +0900

    가로글자흐

--- js/cube.js
+++ js/cube.js
@@ -30,7 +30,7 @@ const slideScreen = () =>{
 }
 
 const rollCube = ()=>{            
-    cube.style.transform = `rotateX(${-40*page}deg)`    
+    cube.style.transform = `rotateY(${-40*page}deg)`    
 }
 
 const paintScrollBar = (scrollPer)=>{
