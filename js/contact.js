1567ce02dc220e6d6d66a40ffa3e6fc4d45a58fb
ouo_yoonk <joyk9251@gmail.com>
2021-03-10 12:10:17 +0900

    이메일 복사

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,10 @@
+const msg = document.querySelector('.speech-bubble')
 var clipboard = new ClipboardJS('.copy-btn');
     clipboard.on('success', function(e) {
-        console.log(e);
+        msg.style.display = 'block'
+        setTimeout(()=>{
+            msg.style.display = 'none'
+        },1000)
     });
     clipboard.on('error', function(e) {
         console.log(e);
